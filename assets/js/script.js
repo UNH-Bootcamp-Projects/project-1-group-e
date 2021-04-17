fd339ec7843f762d9eb3ab3ccc0ed41aea81a854
LB Vik <laura.bullek@gmail.com>
2021-04-17 12:05:57 -0400

    Initialized the navbar and got it to work!

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,11 @@
 $(function (){
     function init() {
         console.log('%c Init', 'background-color: #333; color: #FFE849;');
+        // Initialize collapse button
+        $(document).ready(function(){
+            $('.sidenav').sidenav();
+          });
     }
-
+    
     init();
 });
